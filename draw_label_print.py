589b29db1bc3ea4990a8b184d4988a2fe8386606
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-05-23 16:17:25 +1000

    Update draw_label_print.py

--- draw_label_print.py
+++ draw_label_print.py
@@ -1,5 +1,12 @@
-
+import os
+from datetime import datetime
 import cv2
+import pandas as pd
+
+import global_params_variables
+
+params = global_params_variables.ParamsDict()
+output_audit_path = params.get_value('output_audit_path')
 
 text_y = 25
 
@@ -22,10 +29,46 @@ def is_truck_txt(frame, roi_key, is_truck):
     cv2.putText(frame, f"{roi_key}: {is_truck}", (10, text_y + 50), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (255, 0, 255), 1)
 
 
-def print_results(predictions):
+def format_results(predictions):
     for group in predictions:
         if len(group) > 5:  # assume trucks have a number > 5
+            # the entire result with conf level and x/y of roi
             group_str = ' '.join([f"{number} ({x}, {y}) {confidence:.3f}" for x, y, number, confidence in group])
-            print(group_str)
-            print()
-            print([number for _, _, number, _ in group])
+            trunck_num = [number for _, _, number, _ in group]
+            return [trunck_num, group_str]
+
+
+def print_results(results):
+    print(results[0])
+    print()
+    print(results[1])
+
+
+def save_results(truck_number, video_name):
+    output_dir = os.path.dirname(output_audit_path)
+    os.makedirs(output_dir, exist_ok=True)
+
+    if os.path.exists(output_audit_path):
+        # Read the existing CSV file to find the latest test number
+        df_existing = pd.read_csv(output_audit_path)
+        if not df_existing.empty and 'test_number' in df_existing.columns:
+            last_test_number = df_existing['test_number'].max()
+        else:
+            last_test_number = 0
+    else:
+        last_test_number = 0
+
+    test_number = last_test_number + 1
+    test_time = datetime.now().strftime("%Y-%m-%d %H:%M:%S")
+
+    data = {
+        "date": [test_time],
+        "truck_number": [truck_number],
+        "video_name": [video_name],  # file name, assuming test vids are mp4's
+        "test_number": [test_number]
+    }
+
+    df = pd.DataFrame(data, columns=["date", "truck_number", "video_name", "test_number"])
+    df.to_csv(output_audit_path, index=False, mode='a', header=not os.path.exists(output_audit_path))
+
+

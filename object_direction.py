af31c80cf6c1981bedf107de357db42a24c5b2c8
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-05-24 07:36:51 +1000

    Update object_direction.py

--- object_direction.py
+++ object_direction.py
@@ -17,15 +17,15 @@ def calculate_optical_flow(raw_frame, prev_frame, roi_points):
 
     flow = cv2.calcOpticalFlowFarneback(roi_gray_prev_frame, roi_gray_raw_frame, None, 0.5, 3, 15, 3, 5, 1.2, 0)
     magnitude, angle = cv2.cartToPolar(flow[..., 0], flow[..., 1])
- 
+
     # Check if the average magnitude exceeds the threshold
     direction = ""
     if magnitude.mean() > 0.09:  # Magnitude threshold for significant motion
         if magnitude.mean() > 0:  # Motion towards the right of the screen
-            cv2.putText(raw_frame, "Truck Out", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv2.putText(raw_frame, "Truck Out", (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1)
             direction = "Truck Out"
         elif magnitude.mean() <= 0:  # Motion towards the top of the screen
-            cv2.putText(raw_frame, "Truck In", (50, 50), cv2.FONT_HERSHEY_SIMPLEX, 1, (0, 0, 255), 2)
+            cv2.putText(raw_frame, "Truck In", (25, 25), cv2.FONT_HERSHEY_SIMPLEX, 0.7, (0, 0, 255), 1)
             direction = "Truck In"
 
     return raw_frame, direction

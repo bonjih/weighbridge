c95cd68912d5fbe7324231e91b5fbbe8538809c8
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-05-26 10:48:04 +1000

    Update main.py

--- main.py
+++ main.py
@@ -2,19 +2,20 @@ import cv2
 
 from RoiMultiClass import ComposeROI
 import global_params_variables
-from utils import print_results, format_results, save_results
-from hf_mnist import predict_numbers
-from process_truck import process_images_dir, group_predictions_sort
+from process_frame import func_call
 from stream_video import VideoStreamer
+from utils import clear_directory
 
 params = global_params_variables.ParamsDict()
 configs = ComposeROI(params.get_all_items())
 video_path = configs.video_file
 offset = params.get_value('offset')  # start video at a ts
+is_save_video = params.get_value('is_save_video')
+output_roi_path = params.get_value('output_roi_path')
 
 
 def run():
-    streamer = VideoStreamer.stream_helper(video_path, configs, offset)
+    streamer = VideoStreamer.stream_helper(video_path, configs, offset, is_save_video)
 
     for frame, cam_name in streamer:
         cv2.imshow(cam_name, frame)
@@ -26,15 +27,9 @@ def main():
     print('Processing Video')
     run()
     print('Saving / Segmenting images')
-    ROIs = process_images_dir("saved_frames")
-    print('Running Prediction')
-    predictions = predict_numbers(ROIs)
-    predictions = group_predictions_sort(predictions)
+    func_call()
 
-    # results of prediction
-    results = format_results(predictions)
-    print_results(results)
-    save_results(results[0], video_path)
+    clear_directory(output_roi_path)
 
 
 if __name__ == "__main__":

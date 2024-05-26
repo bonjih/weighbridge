1cb8a7e12d8238a59f0ea1a550640ccdc6e6f092
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-05-26 13:17:55 +1000

    Update process_frame.py

--- process_frame.py
+++ process_frame.py
@@ -1,6 +1,7 @@
 import os
 import cv2
 import numpy as np
+from concurrent.futures import ThreadPoolExecutor
 
 from hf_mnist.hf_mnist import predict_numbers
 from object_direction import calculate_optical_flow
@@ -31,7 +32,6 @@ def func_call():
     print('Running Prediction')
     predictions = predict_numbers(ROIs)
     predictions = group_predictions_sort(predictions)
-    # results of prediction
     print('Formatting / Saving Results')
     results = format_results(predictions)
     print_results(results)
@@ -40,67 +40,59 @@ def func_call():
 
 class FrameProcessor:
     def __init__(self, roi_comp):
-        self.truck_detected_frame_index = None
         self.roi_comp = roi_comp
-        self.motion_frames = {key: 0 for key in roi_comp.rois}
-        self.motion_start_frame = {key: 0 for key in roi_comp.rois}
         self.truck_detected = False
         self.directions = {}
         self.ocr_results = {}
+        self.model_path = model_path
+        self.classes_path = classes_path
+        self.colors_path = colors_path
+        self.segmentation_perc = segmentation_perc
+        self.output_image_path = output_image_path
+        os.makedirs(self.output_image_path, exist_ok=True)
+        self.executor = ThreadPoolExecutor(max_workers=4)
+
+    def segment_image(self, raw_frame, frame_index):  # to segment while streaming video
+        segmenter = Segmenter(self.model_path, self.classes_path, self.colors_path)
+        pct, image = segmenter.process_images(raw_frame)
+        if pct < self.segmentation_perc:
+            img_name = f"frame_{frame_index}.jpg"
+            img_path = os.path.join(self.output_image_path, img_name)
+            cv2.imwrite(img_path, image)
+            _, ocr_result = _ocr(raw_frame)
+            self.ocr_results[frame_index] = ocr_result
+            t_name.append(ocr_result)
+            directions.append(self.directions[frame_index])
+        return raw_frame
 
     def process_frame(self, raw_frame, prev_frame, frame_height, frame_width, ts, back, frame_index):
-        os.makedirs(output_image_path, exist_ok=True)
         timestamp(raw_frame, ts)
-
-        # ensure that self.truck_detected remains True if any ROI detects a truck
         truck_detected_in_frame = False
 
         for roi_key in self.roi_comp.rois:
             roi = self.roi_comp.rois[roi_key]
             roi_points = roi.get_polygon_points()
-
-            # draw_roi_poly(raw_frame, roi_key, roi_points)
-
-            # Extract the bounding box of the ROI
             x, y, w, h = cv2.boundingRect(np.array(roi_points))
             roi_frame = raw_frame[y:y + h, x:x + w]
             prev_roi_frame = prev_frame[y:y + h, x:x + w]
 
-            # Movement detection
             masking_instance = RegionMasking(roi_points)
             fgmask_1, fgmask_2, region = masking_instance.masking(prev_frame, raw_frame, back)
             thresh_type = masking_instance.threshold(fgmask_1, fgmask_2)
 
-            # Process detection
             truck_instance = TruckDetector(thresh_type, frame_width, frame_height, roi_key, region, frame_index)
             frame_is_truck, is_truck = truck_instance.is_truck(raw_frame)
 
             if is_truck:
                 truck_detected_in_frame = True
-                # can use roi_key as truck in or out
                 raw_frame, direction = calculate_optical_flow(raw_frame, prev_frame, roi_points)
-                self.directions[frame_index] = direction  # get the direction for saving to audit csv
+                self.directions[frame_index] = direction
 
-                if truck_detected_in_frame and frame_index % 30 == 0:  # write every n frames
-                    img_name = f"frame_{frame_index}.jpg"
-                    img_path = os.path.join(output_image_path, img_name)
-
-                    # Process segmentation
-                    # if black is < some percent, save image for number prediction
-                    # get the best image for prediction
-                    segmenter = Segmenter(model_path, classes_path, colors_path)
-                    pct, image = segmenter.process_images(raw_frame)
-
-                    if pct < segmentation_perc:
-                        directions.append(list(self.directions.items())[0][1])
-                        cv2.imwrite(img_path, image)
-                        raw_frame, ocr_result = _ocr(raw_frame)
-                        self.ocr_results[frame_index] = ocr_result
-                        t_name.append(list(self.ocr_results.items())[0][1])
+                if frame_index % 30 == 0:
+                    self.executor.submit(self.segment_image, raw_frame, frame_index)
 
             if is_truck:
                 truck_number(raw_frame, t_name)
 
         self.truck_detected = truck_detected_in_frame
-
         return raw_frame

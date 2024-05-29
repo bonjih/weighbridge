adf7027670018c2fb892f49cb1816728458d7572
Ben Hamilton <37001472+bonjih@users.noreply.github.com>
2024-05-30 07:44:50 +1000

    Update is_truck.py

--- is_truck.py
+++ is_truck.py
@@ -8,32 +8,22 @@ class TruckDetector:
     is_truck - if any pixels match the threshold in the ROI, a flag is raised True/False.
     """
 
-    def __init__(self, thresh_type, w, h, roi, roi_is_truck, frame_index):
+    def __init__(self, thresh_type, w, h, roi_key, roi_is_truck, frame_index):
         self.thresh_type = thresh_type
         self.w = w
         self.h = h
-        self.roi = roi
+        self.roi_key = roi_key
         self.roi_is_truck = roi_is_truck
         self.frame_index = frame_index
 
     def is_truck(self, frame_orig):
+        thresh_resized = cv2.resize(self.thresh_type, (self.w, self.h), interpolation=cv2.INTER_LINEAR)
+        current_frame_temp = frame_orig.copy()
+        current_frame_temp[thresh_resized == 255] = (255, 255, 255)
 
-        if self.frame_index > 600:  # adjust to when is_truck / start writing images
-
-            thresh_resized = cv2.resize(self.thresh_type, (self.w, self.h), interpolation=cv2.INTER_LINEAR)
-            current_frame_temp = frame_orig.copy()
-            current_frame_temp[thresh_resized == 255] = (255, 255, 255)
-
-            if len(current_frame_temp[thresh_resized == 255]) > 1000:
-                cv2.putText(current_frame_temp, self.roi, (self.roi_is_truck[1][0] - 15, self.roi_is_truck[1][1] - 10),
-                            cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
-                return current_frame_temp, True
-            else:
-                return frame_orig, False
+        if len(current_frame_temp[thresh_resized == 255]) > 1000:
+            cv2.putText(current_frame_temp, self.roi_key, (self.roi_is_truck[1][0] - 15, self.roi_is_truck[1][1] - 10),
+                        cv2.FONT_HERSHEY_SIMPLEX, 0.5, (255, 255, 255), 1)
+            return current_frame_temp, True
         else:
             return frame_orig, False
-
-
-
-
-
